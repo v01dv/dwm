1fd7bdd036f7250858b41cac260f94bf14230453
voldv <gt.en72v@simplelogin.co>
2025-01-23 16:59:35 +0200

    fix: remove define LENGTH(X) because in the main it is already present into util.h

--- dwm.c
+++ dwm.c
@@ -60,7 +60,6 @@
 #define ISINC(X)                ((X) > 1000 && (X) < 3000)
 #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]))
 #define PREVSEL                 3000
-#define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOD(N,M)                ((N)%(M) < 0 ? (N)%(M) + (M) : (N)%(M))
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
